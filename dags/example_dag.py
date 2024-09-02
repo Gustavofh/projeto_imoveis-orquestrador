5487a38a8887ec2208395b946dfdfd5568da069a
Gustavofh <gustavofh94@gmail.com>
2024-09-02 13:49:45 -0300

    creating airflow plugin

--- dags/example_dag.py
+++ dags/example_dag.py
@@ -2,41 +2,43 @@ from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 from airflow.operators.bash import BashOperator
 from airflow.models import Variable
+from print_execution_date_plugin import PrintExecutionDateOperator
+
 import os
+import logging
 
-# Definindo o diretório onde o repositório será clonado
 CLONE_DIR = ' /opt/airflow/downloads/projeto_imoveis-coleta'
-
 GITHUB_TOKEN = Variable.get('GITHUB_TOKEN')
 
 @dag(schedule=None, start_date=days_ago(1), catchup=False)
 def example_dag():
     
+    print_execution_date = PrintExecutionDateOperator(
+        task_id='print_execution_date'
+    )
+    
     @task.bash
     def clone_repository():
-        # Comando para clonar o repositório GitHub
         repo_url = f'https://{GITHUB_TOKEN}@github.com/Gustavofh/projeto_imoveis-coleta.git'
         clone_command = f'git clone {repo_url} {CLONE_DIR}'
         return clone_command
     
     @task.bash
     def process_data():
-        print('Processando dados...')
+        logging.info('Processing...')
         process_command = f'python {CLONE_DIR}/main.py'
         return process_command
     
     @task.bash
     def cleanup():
-        # Limpeza após o processamento
-        print(f'Removendo diretório {CLONE_DIR}...')
+        logging.info(f'Removing {CLONE_DIR} repo...')
         return f'rm -rf {CLONE_DIR}'
     
-    # Definindo a ordem de execução das tarefas
     clone_task = clone_repository()
     process_task = process_data()
     cleanup_task = cleanup()
 
-    clone_task >> process_task >> cleanup_task
+    print_execution_date >> clone_task >> process_task >> cleanup_task
 
 # Instanciando a DAG
 dag = example_dag()
